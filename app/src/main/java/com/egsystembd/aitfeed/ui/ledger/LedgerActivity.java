5ebc40e0e56493a1e4a9e8a109a6ac9f91ea0eaa
RashedRifat88 <rasrif88@gmail.com>
2022-09-22 17:52:54 +0600

    drawer folders...

---
+++ app/src/main/java/com/egsystembd/aitfeed/ui/ledger/LedgerActivity.java
@@ -0,0 +1,16 @@
+package com.egsystembd.aitfeed.ui.ledger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.egsystembd.aitfeed.R;
+
+public class LedgerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ledger);
+    }
+}
